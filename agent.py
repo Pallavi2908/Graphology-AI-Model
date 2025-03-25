a0299d3885aff9f78dd0adfe9513621d69d297cf
Pallavi2908 <99528124+Pallavi2908@users.noreply.github.com>
2025-03-25 15:57:29 +0530

    Added relative path

--- agent.py
+++ agent.py
@@ -2,29 +2,26 @@ import os
 from mistralai import Mistral
 from constants import Mistral_apikey
 
-def get_traits_from_AI(features : dict):
-    SYSTEM_PROMPT=""
-    with open(r"C:\Users\Rajesh Sinha\Desktop\for_backup\Psyfer-React\psyfer\Graphology_AI\agent.py", 'r') as prompt_file : 
+def get_traits_from_AI(features: dict):
+    # Get the directory where THIS script (agent.py) is located
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))  
+
+    # Correct relative path
+    file_path = os.path.join(BASE_DIR, "agent.py")  
+
+    SYSTEM_PROMPT = ""
+    with open(file_path, 'r') as prompt_file:  
         SYSTEM_PROMPT = prompt_file.read()
 
     api_key = Mistral_apikey
     model = "mistral-large-latest"
 
     client = Mistral(api_key=api_key)
-    messages = [{
-            "role": "system",
-            "content": f"{SYSTEM_PROMPT}",
-        },
-        {
-            "role": "user",
-            "content": f"The available features for user handwriting are : {str(features)}",
-        }
+    messages = [
+        {"role": "system", "content": f"{SYSTEM_PROMPT}"},
+        {"role": "user", "content": f"The available features for user handwriting are: {str(features)}"},
     ]
-    chat_response = client.chat.complete(
-        model = model,
-        messages = messages,
-        
-    )
 
-    return(chat_response.choices[0].message.content)
+    chat_response = client.chat.complete(model=model, messages=messages)
 
+    return chat_response.choices[0].message.content
